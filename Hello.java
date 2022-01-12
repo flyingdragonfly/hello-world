b584b52a2261397d00144eb2a72e0294602842d4
Ada <69705892+adaext@users.noreply.github.com>
2022-01-12 17:33:32 +0800

    Add "hello world" in Java (#4)
    * Add "hello world" in Java
    
    * Revise Hello.java
    
    * Add JavaDoc
    
    * Add "!" back
    
    Co-authored-by: Ada <fishingfishes@qq.com>

---
+++ Hello.java
@@ -0,0 +1,6 @@
+/** Simple program that prints "Hello, world!" on the screen. */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
